cceeb41dbae6d0024635d43be3a79dd6c264f785
MiMo <michael.a.morris.public@gmail.com>
2020-07-22 22:39:08 -0500

    add Header component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react'
-import { Route, Switch, Link } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
 import './App.css'
 import Home from './pages/Home'
 import Rooms from './pages/Rooms'
 import SingleRoom from './pages/SingleRoom'
 import Error from './pages/Error'
+import Navbar from './components/Navbar'
 
 function App () {
   return (
     <>
+      <Navbar />
       <Switch>
         <Route path='/' exact component={Home} />
         <Route path='/rooms' exact component={Rooms} />
